0c4c6845d591c6e872f832ac761080bf4b772e0f
katiemuto1 <47401192+katiemuto1@users.noreply.github.com>
2019-03-11 16:43:26 -0500

    Add files via upload

---
+++ js/cover.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $(".black").fadeOut("slow").fadeIn("slow").fadeOut("slow");
+  $(".blue").fadeIn("slow").fadeOut("slow").fadeIn("slow");
+  $(".darkgreen").fadeIn("slow").fadeOut("slow").fadeIn("slow");
+  $(".title").fadeIn("slow").fadeOut("slow").fadeIn("slow");
+  $(".dots").fadeIn("slow").fadeOut("slow").fadeIn("slow");
+  $(".lines").fadeIn("slow").fadeOut("slow").fadeIn("slow");
+  $(".lightgreen").fadeIn("slow").fadeOut("slow").fadeIn("slow");
+
+});
